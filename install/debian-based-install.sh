c7f23688d5f139f77203e060580866fb2435df29
Gavin Fox <gavingogaming@gmail.com>
2024-07-13 15:54:52 -0700

    Create debian-based-install.sh

---
+++ install/debian-based-install.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install python3 python3-pip python3-cryptography python3-netifaces -y
+python3 -m pip install -U pip 
+sudo apt install less libxext6 libxrender1 libxtst6 libfreetype6 libxi6 -y  
+pip3 install projector-installer --user
