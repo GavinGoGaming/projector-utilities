599e27f27e10d1895e8831808b192ea31efb2797
Gavin Fox <gavingogaming@gmail.com>
2025-02-09 17:45:46 -0800

    Create csb-based-install.sh

---
+++ install/csb-based-install.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install pipx python3 python3-pip python3-cryptography python3-netifaces -y
+python3 -m pipx install -U pipx
+sudo apt install less libxext6 libxrender1 libxtst6 libfreetype6 libxi6 -y  
+pipx install projector-installer
